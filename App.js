573130f8c241a5b152edb2be5a7cf9666a0326c1
Arbi Velaj <arbi.velaj@gmail.com>
2021-02-09 15:07:40 +0100

    first

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { WebView } from "react-native-webview";
+
+export default function App() {
+  return (
+    <WebView
+      style={{ marginTop: 20 }}
+      source={{ uri: "https://www.mycreditagency.it" }}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
